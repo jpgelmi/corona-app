f15136d2db566b5b6fcf4c2a834d08c03936f08a
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-10-17 17:56:10 -0300

    commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, ScrollView, SafeAreaView, View} from 'react-native'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export default {
+    primario: "#2E279D",
+    secundario: "#4D80E4",
+    azul_agua: "#46B3E6",
+    azul_claro: "#ACE5FF",
+    blanco: "#FFFFFF"
+}
